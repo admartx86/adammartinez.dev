cf417be19cc9ad28cb50e2c0d4096595e32662dc
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-01 15:47:03 -0500

    Build: Add and configure dependencies

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './css/styles.css';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+);
