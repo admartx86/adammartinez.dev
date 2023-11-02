ff90cafcd100f05fc451314ce5b2cb0ed89f764d
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-01 19:56:01 -0500

    feat: Add Figtree font

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,18 @@ module.exports = {
                 test: /\.css$/i,
                 include: path.resolve(__dirname, 'src'),
                 use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'fonts/'
+                    }
+                  }
+                ]
               }
         ]
     },
