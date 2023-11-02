77bd075217d67cd4ab0956ae8b1ecb14f8c0de1c
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-01 21:30:14 -0500

    feat: Add header to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,10 @@ import Skills from './Skills'
 const Home = () => {
     return (
         <div>
+            <div className='p-5 flex flex-col'>
+                <h1 className='text-2xl text-center p-1'>Adam Martinez</h1>
+                <h1 className='text-4xl font-bold text-center p-1'>JUNIOR WEB DEVELOPER</h1>
+            </div>
             <Skills />    
         </div>
     )
