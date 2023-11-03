6e1d289fbd0ba10a372956a4fae5d3a5669bb761
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-03 03:03:54 -0500

    refactor: Format text in Portfolio for larger screens

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,7 @@ const Portfolio = () => {
     return(
         <div className='flex flex-col lg:text-xl'>
              <h2 className='text-2xl md:text-3xl lg:text-4xl p-5 self-center'>Portfolio</h2>
+             <div className='md:w-2/3 self-center'>
             <p className='font-bold px-5 py-2'>Summit Styles</p>
             <p className='px-5 py-2'>A simple full-stack e-commerce storefront built with React, Linode, Node.js, Express, MongoDB, and Amazon S3</p>
             
@@ -25,12 +26,14 @@ const Portfolio = () => {
         View back-end source code
     </a>
 
-
+</div>
 
              <video className='md:w-3/4 p-5 self-center shadow-lg' controls>
       <source src={'https://s3.us-east-2.amazonaws.com/adammartinez.dev/summit-styles-demo-1.mkv'} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
+
+    <div className='md:w-2/3 self-center'>
     <p className='font-bold px-5 py-2'>Catbook</p>
     <p className='px-5 py-2'>A simple full-stack Twitter clone built with React, Redux, Tailwind CSS, Linode, Node.js, Express, MongoDB, and Amazon S3</p>
     
@@ -51,7 +54,7 @@ const Portfolio = () => {
     className="block px-5 py-2 underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
         View back-end source code
     </a>
-    
+    </div>
     <video className='md:w-3/4 p-5 self-center shadow-lg' controls>
       <source src={'https://s3.us-east-2.amazonaws.com/adammartinez.dev/catbook-demo-1.mkv'} type="video/mp4" />
       Your browser does not support the video tag.
