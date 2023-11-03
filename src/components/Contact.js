22980a817c654c4f0e42c0d7c6b641e8bc569441
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-03 00:46:38 -0500

    feat: Add social media links in Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,6 +30,43 @@ const Contact = ()  => {
       </div>
     </div>
          
+         <div className='flex justify-center'>
+         <div className='flex flex-col items-center p-5'>
+                    <a href="https://github.com/admartx86"
+                    target="_blank">
+                    <img
+                    src="https://s3.us-east-2.amazonaws.com/adammartinez.dev/GitHub.png"
+                    alt="GitHub"
+                    title="GitHub"
+                    className='w-20 hover:scale-110 transition-all ease-in-out duration-200'
+                    />
+                    </a>
+                </div>
+
+                <div className='flex flex-col items-center p-5'>
+                    <a href="https://www.twitter.com/admartx86"
+                    target="_blank">
+                    <img
+                    src="https://s3.us-east-2.amazonaws.com/adammartinez.dev/Twitter.png"
+                    alt="Twitter"
+                    title="Twitter"
+                    className='w-20 hover:scale-110 transition-all ease-in-out duration-200'
+                    />
+                    </a>
+                </div>
+
+                <div className='flex flex-col items-center p-5'>
+                    <a href="https://www.linkedin.com/in/admartx86"
+                    target="_blank">
+                    <img
+                    src="https://s3.us-east-2.amazonaws.com/adammartinez.dev/LinkedIn.png"
+                    alt="LinkedIn"
+                    title="LinkedIn"
+                    className='w-20 hover:scale-110 transition-all ease-in-out duration-200'
+                    />
+                    </a>
+                </div>
+         </div>
     </div>
     )
 }
