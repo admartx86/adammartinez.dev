ec85684fe83276a73c52e0e3f9c52d51d81a87e5
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-02 23:51:08 -0500

    feat: Add HTTP server on PORT 3000

---
+++ adammartinez-server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000 (HTTP)');
+});
